b26f12fb78e92ba898e4320ee0830312aee09ebc
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-06-29 11:47:29 +0530

    Update BusinessComponent.js

--- src/components/BusinessComponent.js
+++ src/components/BusinessComponent.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 import HeroSection from './sections/HeroSection';
 import VideoSection from './sections/VideoSection';
 import InfoSection from './sections/InfoSection';
-import PricingSection from './sections/PricingSection';
-import ServicesSection from './sections/ServicesSection';
+
 import TemplateSection from './sections/TempletSection';
 
 const HomeContainer = styled.div`
