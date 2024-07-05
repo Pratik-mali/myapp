abdabb9c4b3e7237b3134515b2bc57629ccf9a00
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-07-05 15:20:42 +0530

    Update BusinessComponent.js

--- src/components/BusinessComponent.js
+++ src/components/BusinessComponent.js
@@ -61,10 +61,11 @@ const BusinessComponent = () => {
           <div className="hero-text">
             <h1>प्रभावी बायोडाटा सेवांसाठी तुमची पहिली पसंती! <br></br>'mazabiodata.com'</h1>
             <p>Find and customize the perfect template for your needs</p>
+    <div className="myButton">
             <button className="scroll-button" onClick={handleScrollToTemplates}>Explore Templates</button>
             <button className="scroll-button" onClick={handleScrollToTemplates}>With Photo</button>
             <button className="scroll-button" onClick={handleScrollToTemplates}>Without Photo</button>
-            
+            </div>
           </div>
           <img src="/starr.png" alt="Rotating" className="rotating-image" />
         </div>
