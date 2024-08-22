da71ba3cbb62b541f200ba00a53f14cddbcb47e0
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-08-22 23:50:29 +0530

    Add files via upload

---
+++ src/components/BusinessComponent.js
@@ -0,0 +1,174 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './BusinessComponent.css';
+import FeedbackSlider from './FeedbackSlider';
+import QuoteSlideshow from './QuoteSlideshow';
+
+const BusinessComponent = () => {
+  const navigate = useNavigate();
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [previewTemplate, setPreviewTemplate] = useState(null);
+
+  const templates = [
+    { id: 1, category: 'Hindu', imageUrl: './Marriage Biodata Template-01.png', bestSeller: true },
+    // { id: 23, category: 'Muslim', imageUrl: './Marriage Biodata Template-23.png', bestSeller: false },
+    // { id: 24, category: 'Muslim', imageUrl: './Marriage Biodata Template-24.png', bestSeller: false },
+    // { id: 25, category: 'Muslim', imageUrl: './Marriage Biodata Template-25.png', bestSeller: false },
+    // { id: 26, category: 'Muslim', imageUrl: './Marriage Biodata Template-26.png', bestSeller: false },
+    // { id: 27, category: 'Muslim', imageUrl: './Marriage Biodata Template-27.png', bestSeller: false },
+    { id: 2, category: 'Muslim', imageUrl: './Marriage Biodata Template-02.png', bestSeller: false },
+    { id: 3, category: 'Hindu', imageUrl: './Marriage Biodata Template-03.png', bestSeller: false },
+    { id: 4, category: 'Muslim', imageUrl: './Marriage Biodata Template-04.png', bestSeller: false },
+    { id: 5, category: 'Jain', imageUrl: './Marriage Biodata Template-05.png', bestSeller: true },
+    { id: 6, category: 'Hindu', imageUrl: './Marriage Biodata Template-06.png', bestSeller: false },
+    { id: 7, category: 'Baudhha', imageUrl: './Marriage Biodata Template-07.png', bestSeller: false },
+    { id: 8, category: 'Jain', imageUrl: './Marriage Biodata Template-08.png', bestSeller: false },
+    { id: 14, category: 'Jain', imageUrl: './Marriage Biodata Template-14.png', bestSeller: false },
+    { id: 15, category: 'Hindu', imageUrl: './Marriage Biodata Template-15.png', bestSeller: false },
+    { id: 9, category: 'Baudhha', imageUrl: './Marriage Biodata Template-09.png', bestSeller: false },
+    { id: 10, category: 'Hindu', imageUrl: './Marriage Biodata Template-10.png', bestSeller: false },
+    { id: 11, category: 'Baudhha', imageUrl: './Marriage Biodata Template-11.png', bestSeller: false },
+    { id: 12, category: 'Hindu', imageUrl: './Marriage Biodata Template-12.png', bestSeller: false },
+    { id: 13, category: 'Muslim', imageUrl: './Marriage Biodata Template-13.png', bestSeller: false },
+   
+    { id: 16, category: 'Muslim', imageUrl: './Marriage Biodata Template-16.png', bestSeller: false },
+    // { id: 17, category: 'Jain', imageUrl: './Marriage Biodata Template-17.png', bestSeller: false },
+    // { id: 18, category: 'Muslim', imageUrl: './Marriage Biodata Template-18.png', bestSeller: false },
+    // { id: 19, category: 'Muslim', imageUrl: './Marriage Biodata Template-19.png', bestSeller: false },
+    // { id: 20, category: 'Muslim', imageUrl: './Marriage Biodata Template-20.png', bestSeller: false },
+    // { id: 21, category: 'Muslim', imageUrl: './Marriage Biodata Template-21.png', bestSeller: false },
+    // { id: 22, category: 'Muslim', imageUrl: './Marriage Biodata Template-22.png', bestSeller: false },
+  
+
+  ];
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleCustomizeClick = (template) => {
+    navigate(`/input-form/${template.id}`);
+
+  };
+
+  const handlePreviewClick = (template) => {
+    setPreviewTemplate(template);
+  };
+
+  const handleClosePreview = () => {
+    setPreviewTemplate(null);
+  };
+
+  const handleScrollToTemplates = () => {
+    document.getElementById('templates-section').scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const filteredTemplates = selectedCategory === 'All'
+    ? templates
+    : templates.filter((template) => template.category === selectedCategory);
+
+  return (
+    <div className="business-container">
+      <section className="hero-section">
+        <div className="hero-image">
+          <div className="hero-text">
+            <h1>बायोडेटा साठी प्रत्येकाची पहिली पसंती....! <br/><span className='brandname'>MAZABIODATA.COM</span></h1>
+            <p className='brandpara'>Find and customize the perfect template for your needs</p>
+            <div className="myButton">
+              <button className="scroll-button" onClick={handleScrollToTemplates}>फोटोसह</button>
+              <button className="scroll-button" onClick={handleScrollToTemplates}>फोटोशिवाय</button>
+            </div>
+          </div>
+          {/* <img src="/starr.png" alt="Rotating" className="rotating-image" /> */}
+        </div>
+      </section>
+      <section className="quotes-section">
+        <QuoteSlideshow />
+      </section>
+
+      <section className="about-section1">
+        <div className='impdiv'>
+        <h1>बायोडाटा <span className='ka'>का</span> महत्त्वाचे?</h1>
+
+        <p>
+          सुखी आणि संपन्न आयुष्यासाठी प्रत्येकाला एका चांगल्या जोडीदाराची आवश्यकता असते. जोडीदाराचे व्यक्तिमत्व त्याच्यातले कलागुण आपल्याला माहिती होण्यासाठी एक योग्य बायोडाटा असणे गरजेचे असते. तुम्ही आमच्या प्लॅटफॉर्मच्या माध्यमातून योग्यरीत्या तुमचा बायोडाटा बनवू शकता.<br/><br/>
+          <b>माझा बायोडाटा</b> वेबसाईट वरती सगळी माहिती योग्यरीत्या भरता यावी याची आम्ही काळजी घेतली आहे. 
+          तुमचा बायोडाटा अधिक आकर्षक आणि दर्जेदार असावा यासाठी आम्ही प्रयत्न केला आहे.<br/><br/>
+          सोशल मीडियाच्या माध्यमातून आज मोठ्या प्रमाणामध्ये आपल्याला अनेक गोष्टींची ओळख सहजरीत्या होते. आपण व्हाट्सअप वरील लिंकच्या माध्यमातून अगदी सहजरीच्या आपला बायोडाटा योग्यरीत्या व आकर्षक बनवू शकता. अत्यंत किफायतशीर किमतीमध्ये आपल्याला सेवेचा लाभ घेता येईल.
+        </p>
+       
+        </div>
+      
+      </section>
+
+      <section className="about-section2">
+      <div className='impdiv'>
+
+        <p>
+          1. आपणास सर्व प्रथम mazabiodata.com असे गूगल वरती सर्च करायचं आहे नंतर तुम्हाला ही वेबसाईट ओपन होईल <br/>
+          2. नंतर तुम्ही our templetes मधून सुसज्ज आणि सुंदर असे टेम्प्लेट select करायचं आहे.<br/>
+          3. नंतर तुम्ही टेम्प्लेट चा खाली दिलेल्या वापरा ह्या बटनवर क्लिक करायचं आहे.<br/>
+          4. नंतर तुम्ही next page वरती जाल मग तुम्हाला तुमची सर्व माहिती भरायची आहे उदा. नाव, वडिलांचे नाव.<br/>
+          5. तुमची सर्व माहिती भरत असताना तुम्ही इंग्लिश keyboard वरून spelling type केलं तरी तिथे मराठी मध्ये येईल<br/>
+          6. तुमची सर्व माहिती भरून झाल्यानंतर तुम्ही preview वरती क्लिक करून तुमचा बायोडाटा पाहू शकता.<br/>
+          7. तुम्हाला जर वॉटरमार्क शिवाय बायोडाटा डाऊनलोड करायचं असेल तर तुम्ही download without watermark button वरती क्लिक करा. मग तुमचे फोन पेय किव्वा गूगल पेय वरून 49 rs pay करा <br/>
+          8. मग तुमचा Biodata download होईल
+        </p>
+        <h1>अगदी <span className='ka'>सहज </span> उपयोगी</h1>
+
+        </div>
+
+      </section>
+
+      <section className="templates-section" id="templates-section">
+        <h2>Our Templates</h2>
+        <div className="filter-buttons">
+          <button
+            className={selectedCategory === 'All' ? 'active' : ''}
+            onClick={() => handleCategoryChange('All')}
+          >
+            All
+          </button>
+          {Array.from(new Set(templates.map(template => template.category))).map((category) => (
+            <button
+              key={category}
+              className={selectedCategory === category ? 'active' : ''}
+              onClick={() => handleCategoryChange(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <div className="template-options">
+          {filteredTemplates.map((template) => (
+            <div key={template.id} className="template-option" onClick={() => handlePreviewClick(template)}>
+              {template.bestSeller && <div className="best-seller">Best Seller</div>}
+              <img src={template.imageUrl} alt={`Template ${template.id}`} />
+              {/* <p className="template-description">Temp {template.id}.</p> */}
+              <div className="template-buttons">
+                <button className="template-button" onClick={() => handlePreviewClick(template)}>
+                  <i className="fas fa-eye"></i>
+                </button>
+                <button className="template-button" onClick={() => handleCustomizeClick(template)}>वापरा</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {previewTemplate && (
+        <div className="preview-modal">
+          <div className="preview-content">
+            <button className="close-button" onClick={handleClosePreview}>✖</button>
+            <img src={previewTemplate.imageUrl} alt={`Template ${previewTemplate.id}`} />
+            <button className="template-button" onClick={() => handleCustomizeClick(previewTemplate)}>Customize</button>
+          </div>
+        </div>
+      )}
+      
+      <FeedbackSlider />
+    </div>
+  );
+};
+
+export default BusinessComponent;
---
+++ src/components/TermsAndConditions.js
@@ -0,0 +1,58 @@
+// TermsAndConditions.js
+import React from 'react';
+
+const TermsAndConditions = () => {
+  return (
+    <div style={styles.pageContainer}>
+        <h1>Terms and Conditions for MazaBiodata</h1>
+    <p><strong>Effective Date: 30/07/2024</strong></p>
+
+    <h2>1. Introduction</h2>
+    <p>Welcome to MazaBiodata. By using our service, you agree to comply with these terms. If you disagree with any part, do not use our service.</p>
+
+    <h2>2. Use of Our Services</h2>
+    <p>By using our services, you agree to:</p>
+    <ul>
+        <li>Comply with all applicable laws and regulations.</li>
+        <li>Not use our services for any unlawful or prohibited activities.</li>
+        <li>Provide accurate and complete information when creating or editing a biodata.</li>
+        <li>Respect our intellectual property rights.</li>
+        <li>Refrain from attempting to disrupt or interfere with our services.</li>
+    </ul>
+
+    <h2>3. Payment</h2>
+    <p>All payments must be made in full at the time of the transaction. We accept various forms of payment, including credit/debit cards and online payment methods. Any applicable taxes and fees will be added to your total bill. Payments are processed securely through our third-party payment gateways.</p>
+
+    <h2>4. Refunds</h2>
+    <p>All payments are final and non-refundable, except in cases of technical issues as outlined in our Privacy Policy. If you experience a technical issue, contact us within 24 hours for a possible refund. Refunds are processed using the same payment method as the original transaction.</p>
+
+    <h2>5. Limitation of Liability</h2>
+    <p>To the fullest extent permitted by law, MazaBiodata shall not be liable for any indirect, incidental, special, or consequential damages arising out of or in connection with your use of our website or services. This includes, but is not limited to, loss of data, loss of profits, or any other loss or damage arising from the use or inability to use our services.</p>
+
+    <h2>6. Intellectual Property</h2>
+    <p>All content on our website, including text, graphics, logos, and images, is the property of MazaBiodata or its licensors and is protected by intellectual property laws. You may not use any content from our website without our express written permission.</p>
+
+    <h2>7. Changes to These Terms and Conditions</h2>
+    <p>We may update these terms and conditions from time to time. Any changes will be posted on this page, and we will notify you of significant changes by email or through our website. Your continued use of our services after such changes constitutes your acceptance of the new terms and conditions.</p>
+
+    <h2>8. Governing Law</h2>
+    <p>These terms and conditions are governed by and construed in accordance with the laws of [Insert Jurisdiction]. Any disputes arising from or in connection with these terms and conditions shall be subject to the exclusive jurisdiction of the courts of [Insert Jurisdiction].</p>
+
+    <h2>9. Contact Us</h2>
+    <p>If you have any questions or concerns about these terms and conditions, please contact us at:</p>
+    <p>MazaBiodata<br></br>+91 77769 14543</p>
+    </div>
+  );
+};
+
+const styles = {
+  pageContainer: {
+    padding: '20px',
+    maxWidth: '800px',
+    margin: '0 auto',
+  marginTop: '11rem'
+    
+  },
+};
+
+export default TermsAndConditions;
