607142aea1cbf3a64f30e80880b6f90abd6def31
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-08-23 15:40:24 +0530

    Add files via upload

---
+++ src/components/ContactUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContactUs.css'; // Import CSS for styling
+
+const Contact = () => {
+  return (
+    <div className="contact-card">
+      <h2 className="contact-name">mazabiodata.com</h2>
+      <p className="contact-email">Email: mazabiodata@gmail.com</p>
+      <p className="contact-phone">Phone: +91 77769 14543s</p>
+    </div>
+  );
+};
+
+export default Contact;
