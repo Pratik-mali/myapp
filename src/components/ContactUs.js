53b7cd6745b07fc67bc628df77d3fa9cf572f393
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-08-24 18:26:40 +0530

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,6 +7,7 @@ const Contact = () => {
       <h2 className="contact-name">mazabiodata.com</h2>
       <p className="contact-email">Email: mazabiodata@gmail.com</p>
       <p className="contact-phone">Phone: +91 77769 14543s</p>
+      <p className="">Address: More Vasti, At-Po: Tulsi, Tal-Madha, Dist-Solapur, 413302</p>
     </div>
   );
 };
