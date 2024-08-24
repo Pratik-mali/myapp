c7580f0bde4ddeca915938810de6813876abff29
Pratik Mali <75832144+Pratik-mali@users.noreply.github.com>
2024-08-24 18:32:13 +0530

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -6,7 +6,7 @@ const Contact = () => {
     <div className="contact-card">
       <h2 className="contact-name">mazabiodata.com</h2>
       <p className="contact-email">Email: mazabiodata@gmail.com</p>
-      <p className="contact-phone">Phone: +91 77769 14543s</p>
+      <p className="contact-phone">Phone: +91 77769 14543</p>
       <p className="">Address: More Vasti, At-Po: Tulsi, Tal-Madha, Dist-Solapur, 413302</p>
     </div>
   );
